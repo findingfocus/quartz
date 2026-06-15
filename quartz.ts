75c314c9445b719cce994178ff45912b959345b8
Paul Thompson <paul@findingfocus.dev>
2026-06-15 10:33:55 -0600

    Quartz sync: Jun 15, 2026, 10:33 AM

--- quartz.ts
+++ quartz.ts
@@ -1,12 +1,6 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
-import { RSSOnlyEmitter } from "./quartz/plugins/emitters/RSSOnlyEmitter"
 
 const config = await loadQuartzConfig()
-config.plugins.emitters = config.plugins.emitters.filter(
-  e => e.name !== "ContentIndex"
-)
-
-config.plugins.emitters.push(RSSOnlyEmitter())
 
 export default config
 export const layout = await loadQuartzLayout()
