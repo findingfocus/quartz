f7b8e0f27830541a0ca49ff0c855b501e804a43e
Paul Thompson <paul@findingfocus.dev>
2026-06-15 12:49:41 -0600

    Quartz sync: Jun 15, 2026, 12:49 PM

---
+++ patch-rss.sh
@@ -0,0 +1,1 @@
+sed -i '' 's/content: generateRSSFeed(cfg, linkIndex, options, options.rssLimit),/content: generateRSSFeed(cfg, new Map(Array.from(linkIndex).filter(([, c]) => c.tags.includes("feed"))), options, options.rssLimit),/' .quartz/plugins/content-index/dist/index.js
--- quartz.ts
+++ quartz.ts
@@ -1,5 +1,6 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
 
+//test
 const config = await loadQuartzConfig()
 
 export default config
